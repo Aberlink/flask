a39253310ae16ecfd53650423a7e15914066f66f
aberlink <pycia.michal@gmail.com>
2022-02-20 17:34:01 +0100

    test

--- forms.py
+++ forms.py
@@ -16,5 +16,8 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    mama
+    
+
 
 #     38:20
